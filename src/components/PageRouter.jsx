f7cbe02364c915bf5597c0bef93116ec6a6d662d
Myint Myat Kaung <42793386+MyintMyatKaungPHP@users.noreply.github.com>
2024-11-22 19:56:08 +0630

    remove link dom

--- src/components/PageRouter.jsx
+++ src/components/PageRouter.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
 import Layout from "./Layout.jsx";
 import Home from "../pages/Home.jsx";
